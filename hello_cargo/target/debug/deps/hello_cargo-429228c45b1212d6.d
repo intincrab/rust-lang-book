47a79f41ecab02e21a9c48d6f1b4447149c70f66
Ajay <exactlyajay.chauhan@gmail.com>
2023-01-27 11:43:25 +0530

    new changes baby

---
+++ hello_cargo/target/debug/deps/hello_cargo-429228c45b1212d6.d
@@ -0,0 +1,5 @@
+C:\Users\azay\Desktop\rust\hello_cargo\target\debug\deps\hello_cargo-429228c45b1212d6.rmeta: src\main.rs
+
+C:\Users\azay\Desktop\rust\hello_cargo\target\debug\deps\hello_cargo-429228c45b1212d6.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\azay\Desktop\rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\azay\Desktop\rust\hello_cargo\src\main.rs
