6177d33f0bcf3237946b6ecb301e193090e7c4e5
Ajay <exactlyajay.chauhan@gmail.com>
2023-01-31 09:36:15 +0530

    functions

---
+++ CommonProgrammingConcepts/Functions/target/debug/Functions.d
@@ -0,0 +1,1 @@
+C:\Users\azay\Desktop\rust\CommonProgrammingConcepts\Functions\target\debug\Functions.exe: C:\Users\azay\Desktop\rust\CommonProgrammingConcepts\Functions\src\main.rs
---
+++ CommonProgrammingConcepts/Functions/target/debug/deps/Functions.d
@@ -0,0 +1,5 @@
+C:\Users\azay\Desktop\rust\CommonProgrammingConcepts\Functions\target\debug\deps\Functions.exe: src\main.rs
+
+C:\Users\azay\Desktop\rust\CommonProgrammingConcepts\Functions\target\debug\deps\Functions.d: src\main.rs
+
+src\main.rs:
