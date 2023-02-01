d130f0b13cb0cce271dfbf9d227721230f7d9fcb
Ajay <exactlyajay.chauhan@gmail.com>
2023-02-01 18:51:34 +0530

    d

---
+++ UnderstandingOwnership/borrowing/target/debug/borrowing.d
@@ -0,0 +1,1 @@
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\borrowing\target\debug\borrowing.exe: C:\Users\azay\Desktop\rust\UnderstandingOwnership\borrowing\src\main.rs
---
+++ UnderstandingOwnership/borrowing/target/debug/deps/borrowing.d
@@ -0,0 +1,5 @@
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\borrowing\target\debug\deps\borrowing.exe: src\main.rs
+
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\borrowing\target\debug\deps\borrowing.d: src\main.rs
+
+src\main.rs:
