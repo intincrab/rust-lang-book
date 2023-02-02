927905346d2ff397589d0f23d1b4d9892e330c47
Ajay <exactlyajay.chauhan@gmail.com>
2023-02-02 19:39:54 +0530

    dd

---
+++ UnderstandingOwnership/slice/target/debug/deps/slice.d
@@ -0,0 +1,5 @@
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\slice\target\debug\deps\slice.exe: src\main.rs
+
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\slice\target\debug\deps\slice.d: src\main.rs
+
+src\main.rs:
---
+++ UnderstandingOwnership/slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+C:\Users\azay\Desktop\rust\UnderstandingOwnership\slice\target\debug\slice.exe: C:\Users\azay\Desktop\rust\UnderstandingOwnership\slice\src\main.rs
